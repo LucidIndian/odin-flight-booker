7193b103dc55961dfb0dca4fc8ef94b6d24fdcbc
Tygh Walters <tyghwalters@gmail.com>
2023-02-20 17:00:27 -0800

    Fix Airport to Flight Associations

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -7,7 +7,7 @@ class Flight < ApplicationRecord
   #               foreign_key: :departure_id,
   #               # inverse_of: :arriving_flights
 
-  belongs_to :departure_airport, class_name: 'Airport', foreign_key: :departure_id, inverse_of: :departure_flights
-  belongs_to :arrival_airport, class_name: 'Airport', foreign_key: :arrival_id, inverse_of: :arrival_flights
+  belongs_to :departure_airport, class_name: 'Airport', foreign_key: :departure_id, inverse_of: :departing_flights
+  belongs_to :arrival_airport, class_name: 'Airport', foreign_key: :arrival_id, inverse_of: :arriving_flights
        
 end
