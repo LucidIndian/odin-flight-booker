8969689d0246c8996bcfb55f097ed65f59cdc49e
Tygh Walters <tyghwalters@gmail.com>
2023-02-21 21:15:55 -0800

    Date Dropdown Shows Unique Flight Days

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,9 +3,8 @@ class FlightsController < ApplicationController
   def index
     @airports = Airport.all
     @flights = Flight.all
-    # datetime.to_fs(:rfc822)        # => "Tue, 04 Dec 2007 00:00:00 +0000"
-    @start_options = @flights.map{ |f| [ f.start.to_fs(:rfc822), f.start ] }.uniq
-  
+    @day_options = @flights.map{ |flight| [ flight.start.day ] }.uniq
+    
   end
 
 end
