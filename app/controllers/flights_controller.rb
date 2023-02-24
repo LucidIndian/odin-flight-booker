e5c9b64f855cedc0f43b94e2452a5a36f1b2ff14
Tygh Walters <tyghwalters@gmail.com>
2023-02-23 22:52:08 -0800

    Search Form Returns Results as Radio Inputs in flights#index
    Still not filtering day yet

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,14 +3,15 @@ class FlightsController < ApplicationController
   def index
     @airports = Airport.all
     @flights = Flight.all
-    @searched_flights = Flight.user_search(query_params)
-    @num_of_passengers = query_params[:passengers_num]
-    @unique_flight_days = @flights.map{ |flight| [ flight.start.day ] }.uniq
-    @day_options = @flights.map{ |flight| [ flight.start.day, flight.start] }.uniq
-    
+    # @num_of_passengers = query_params[:passengers_num]
+    # @unique_flight_days = @flights.map{ |flight| [ flight.start.day ] }.uniq
+    # @day_options = @flights.map{ |flight| [ flight.start.day, flight.start] }.uniq
+    # @searched_flights_testa = Flight.user_search(query_params)
+    @searched_flights_testb = Flight.where(departure_id: params[:departure_id],
+      arrival_id: params[:arrival_id])
+
     # @start_flight_days = Time.now
     # @end_flight_days = Time.now + 5.days
-
   end
 
   private
@@ -18,6 +19,7 @@ class FlightsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def query_params
       params.permit(:departure_id, :arrival_id, :start, :passengers_num, :commit)
+      puts "query_params method ran"
     end
 
 end
