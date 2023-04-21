b0fab128c7073462389da6accd0e6e460ac31815
Tygh Walters <tyghwalters@gmail.com>
2023-04-20 20:30:01 -0700

    Prevent Removal of Last Passenger on Booking#New
    Only concealing the 'Remove passemnger' button when 1 passenger remains, then revealing it otherwise.

--- app/javascript/controllers/passenger_controller.js
+++ app/javascript/controllers/passenger_controller.js
@@ -2,19 +2,21 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
 
-  static targets = [ 'passengerForm', 'passDiv' ];
+  static targets = [ 'passengerForm', 'passDiv', 'removeButton' ];
 
   static values = { passnum: Number }
+
+  static classes = [ "concealed" ] 
 
   connect() {
     // this.element.textContent = "Hello Passenger Controller!"
     console.log("Hello Passenger Controller!");
-    console.log(`passnum value is ${this.passnumValue}`)
+    console.log(`passnumValue is ${this.passnumValue} on connect`)
     // Add the passenger fields the # of times as the params[:num_of_passengers] from the Booking page
     // Set as passnumValue as a different constant so I don't re-write it when I iterate it in the chained addPass() function
     const pnv = this.passnumValue;
     for (let i = 0; i < pnv; i++)  {  
-      // Hack: decrement before incrementing again to offset only when connecting 
+      // Hack: decrement before incrementing again in addPass() below
       this.passnumValue--
       this.addPass();
     }
@@ -31,18 +33,35 @@ export default class extends Controller {
     passDiv.insertAdjacentHTML('beforeend', passengerForm);
     // Iterated passnumValue to update total passengers in the HTML attribute on Booking#New
     this.passnumValue++ 
-    console.log(`passnumValue is ${this.passnumValue}`);
+    console.log(`passnumValue is ${this.passnumValue} after addPass` );
+    this.buttonShow();
   }
 
   removePass() {
     // Grab the target
-    const lastpassDiv = this.passDivTarget;
-    console.log (lastpassDiv.lastChild)
-    lastpassDiv.lastElementChild.remove();
+    const passDiv = this.passDivTarget;
+    console.log (passDiv.lastChild)
+    passDiv.lastElementChild.remove();
     console.log(`removePass ran!`);
     // Decrement passnumValue to update total passengers in the HTML attribute on Booking#New
     this.passnumValue--
-    console.log(`passnumValue is ${this.passnumValue}`);
+    console.log(`passnumValue is ${this.passnumValue} after removePass`);
+    this.buttonShow()
+  }
+
+  buttonShow() {
+    // Show "remove" button w/ 2+ passengers
+    if ( this.passnumValue > 1) {
+      // show the "remove" button!
+      this.element.classList.remove(this.concealedClass)
+      console.log(`REMOVE OK`)
+    } else if ( this.passnumValue < 2) {
+      // conceal the "remove" button!
+      this.element.classList.add(this.concealedClass)
+      console.log(`DO NOT REMOVE`)
+    } else {
+      console.log(`REMOVE BUTTON ERROR`)
+    }
   }
 
 }
