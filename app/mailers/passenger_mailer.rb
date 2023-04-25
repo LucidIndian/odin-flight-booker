1b6126342cc29d134dc4cecdbffed4dbaab92843
Tygh Walters <tyghwalters@gmail.com>
2023-04-24 20:47:00 -0700

    Clean Comments and Space

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -3,14 +3,13 @@ class PassengerMailer < ApplicationMailer
   default from: 'notifications@toptesting.com'
 
   def confirmation_email
-    @booking = params[:booking]
-    # Create an array of emails to send 1 message per passenger
-    @allpassemails = @booking.passengers.map { |pass| pass.email }  
     @url = 'https://example.com/login'
-    
+    @booking = params[:booking]
+    # Create an array to send 1 email per passenger
+    @allpassemails = @booking.passengers.map { |pass| pass.email }  
     mail( to: @allpassemails,
-      subject: 'Your Odin Test Flight is Booked!',
-      booking: @booking)
+      subject: 'Your Odin Test Flight is Booked',
+      booking:  @booking )
   end
 
 end
